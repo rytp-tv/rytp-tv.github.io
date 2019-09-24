7857cb98a073b3edb8fb3539f3f645ab7b0e9a12
rytp-tv <44220764+rytp-tv@users.noreply.github.com>
2019-09-24 21:29:46 +1100

    Add files via upload

---
+++ Chat/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1569320900,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/spritefont.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
