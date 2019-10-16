470c416d375dea3fb00a196f1c671c4fe736d372
rytp-tv <44220764+rytp-tv@users.noreply.github.com>
2019-10-16 14:51:11 +1100

    Add files via upload

---
+++ MyshkinLetter/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1571194396,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/button_legkoslova-sheet0.png",
+		"images/button_slozhnopredlozheniya-sheet0.png",
+		"images/button_sredneslovosochetaniya-sheet0.png",
+		"images/button_trenirovkabukvy-sheet0.png",
+		"images/893f1afef2ef5934e1186111db6d-sheet0.png",
+		"images/pasep015sh-sheet0.png",
+		"images/spritefont.png",
+		"images/sprite-sheet0.png",
+		"images/button_ok-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"media/Сказочный Патруль - Сказочный Патруль - Саундтрек №2 (1).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Вопросы.txt",
+		"jquery.gritter.js",
+		"jquery.gritter.css"
+	]
+}
