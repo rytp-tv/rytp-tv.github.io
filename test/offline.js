4e69ef4f8bcb6ad14ac5a70161730ca84832e962
rytp-tv <44220764+rytp-tv@users.noreply.github.com>
2020-04-19 19:24:43 +1100

    Add files via upload

---
+++ test/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1587284491,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/canvas.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js",
+		"jquery.gritter.js",
+		"jquery.gritter.css"
+	]
+}
